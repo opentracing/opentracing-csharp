e096e400ca0f75d7f606edb7639173fea2110085
dawallin <dawa2323@hotmail.com>
2016-05-26 22:44:09 +0200

    BasicTracer initial commit

---
+++ OpenTracing/OpenTracing.BasicTracer/GuidFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenTracing.BasicTracer
+{
+    internal static class GuidFactory
+    {
+        public static ulong Create()
+        {
+            return BitConverter.ToUInt64(Guid.NewGuid().ToByteArray(), 0);
+        }
+    }
+}
