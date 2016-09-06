9d90a8c2758919eca7747b69867ec248865db33f
dawallin <dawa2323@hotmail.com>
2016-09-07 01:35:01 +0200

    Added IExtract and IInject carrier interfaces.

---
+++ src/OpenTracing/Propagation/IInjectCarrier.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenTracing.Propagation
+{
+    public interface IInjectCarrier<TFormat>
+    {
+        /// <summary>
+        /// MapFrom takes the SpanContext instance and injects it for
+        /// propagation within `carrier`. The actual format of context depends on
+        /// the type of TFormat.
+        /// </summary>
+        void MapFrom(TFormat context);
+    }
+}
