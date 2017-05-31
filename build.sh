29043b1c47a2b43eef27ea9559584a1e4fa76b3d
Christian Weiss <christian@chwe.at>
2017-06-01 00:14:43 +0100

    a (very) simple bash-based build-script (#39)

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# stop on first error
+set -e
+
+# arguments
+
+CONFIGURATION="Release"
+
+# validations
+
+if ! [ -x "$(command -v dotnet)" ]; then
+    echo "dotnet cli is not installed."
+    exit 1
+fi
+
+# tasks
+
+echo
+echo dotnet-restore
+echo ----------------------
+
+dotnet restore
+
+echo
+echo dotnet-build
+echo ----------------------
+
+dotnet build -c $CONFIGURATION --no-incremental
+
+echo
+echo dotnet-test
+echo ----------------------
+
+dotnet test test/OpenTracing.Tests/OpenTracing.Tests.csproj -c $CONFIGURATION --no-build
