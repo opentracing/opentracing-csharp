c6c978d895ab7f7e2e318e898d33c8927b3be408
Christian Weiss <christian@chwe.at>
2018-02-11 10:45:00 +0100

    xml docs

--- src/OpenTracing/Noop/NoopTracerFactory.cs
+++ src/OpenTracing/Noop/NoopTracerFactory.cs
@@ -2,6 +2,9 @@ namespace OpenTracing.Noop
 {
     public static class NoopTracerFactory
     {
+        /// <summary>
+        /// Returns the singleton <see cref="NoopTracer"/> instance.
+        /// </summary>
         public static ITracer Create()
         {
             return NoopTracer.Instance;
