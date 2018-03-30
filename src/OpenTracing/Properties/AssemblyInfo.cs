83de64195ae6e7ac31349bbd98883e93a35c883c
Mike Goldsmith <goldsmith.mike@gmail.com>
2018-03-30 08:18:06 +0100

    Add support for signing assemblies during build (#78) (#80)

--- src/OpenTracing/Properties/AssemblyInfo.cs
+++ src/OpenTracing/Properties/AssemblyInfo.cs
@@ -1,3 +1,8 @@
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("OpenTracing.Tests")]
+[assembly: InternalsVisibleTo("OpenTracing.Tests, PublicKey=" + 
+"00240000048000009400000006020000002400005253413100040000010001005972718f82b630" +
+"d49e0b577dace335f82808461ed43be8ea91047c51305b585c180dfa88574fb17b8be9622e5e13" +
+"d20bcd680c124a6cadb831252df248f2261f6f953da7e0e067062b57a8ae7752cc660fa19512c0" +
+"06544ed0d88aa9471bf0c41e03b085093162e9d15c0de6ebcfedc24f6c256b934c13082fc24ba0" +
+"d705f1a0")]
